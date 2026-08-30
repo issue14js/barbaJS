8b8881bc2e66395cab23e7940d0c027b50ffb1db
Anshu <anshurock989@gmail.com>
2026-08-30 22:30:35 +0530

    add simple transition

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-import './style.css'
+import './main.css'
 import barba from '@barba/core';
+import gsap from 'gsap';
 
 barba.init({
   transitions:[{
     name:'first transition',
-    leave(){
-
+     leave(data){
+       return gsap.to(data.current.container, {
+        opacity:0,
+        duration:1,
+        ease:'power2.inOut'
+        
+      })
+      
     },
-    enter(){
+    enter(data){
+      return gsap.from(data.next.container, {
+        left:0,
+        duration:1,
+        ease:'power2.inOut'
+      })
 
     }
   }]
