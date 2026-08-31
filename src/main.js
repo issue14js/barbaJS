7d8f74389fb8207f851c8823c8fd0597838a2524
Anshu <anshurock989@gmail.com>
2026-08-31 21:48:04 +0530

    add left-to-right animation

--- src/main.js
+++ src/main.js
@@ -5,20 +5,24 @@ import gsap from 'gsap';
 barba.init({
   transitions:[{
     name:'first transition',
-     leave(data){
-       return gsap.to(data.current.container, {
-        opacity:0,
-        duration:1,
-        ease:'power2.inOut'
-        
+    scync:true,
+    leave(data){
+      data.current.container.style.position = 'absolute';
+      return gsap.to(data.current.container, {
+        // opacity:0,
+        x: -1500,
+        duration:0.7,
+        ease:'power1.out'
       })
       
     },
     enter(data){
+      data.current.container.style.position = 'absolute';
       return gsap.from(data.next.container, {
-        left:0,
-        duration:1,
-        ease:'power2.inOut'
+        // opacity:0,
+        x: 1500,
+        duration:0.7,
+        ease:'power1.out'
       })
 
     }
